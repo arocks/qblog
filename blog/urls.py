623b506a61c87cde10c1daf63dd67f4a0a3767a7
Arun Ravindran <arunvr@gmail.com>
2014-12-31 16:17:43 +0530

    the catch all url was creating problems

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     '',
     url(r'^feed/$', feed.LatestPosts(), name="feed"),
     url(r'^$', views.BlogIndex.as_view(), name="index"),
-    url(r'^(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
+    url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
 )
