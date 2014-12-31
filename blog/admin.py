29e38a1f759f97c68d3cdfd2180650acab5a60a7
Arun Ravindran <arunvr@gmail.com>
2014-12-31 19:23:16 +0530

    Added explicit form_overrides for Python 2 users

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from . import models
 from django_markdown.admin import MarkdownModelAdmin
+from django_markdown.widgets import AdminMarkdownWidget
+from django.db.models import TextField
 
 
 class EntryAdmin(MarkdownModelAdmin):
     list_display = ("title", "created")
     prepopulated_fields = {"slug": ("title",)}
+    # Next line is a workaround for Python 2.x
+    formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
 
 admin.site.register(models.Entry, EntryAdmin)
 admin.site.register(models.Tag)
